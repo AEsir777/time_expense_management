204531910bcdce2aefd255cc054939c5f55abbc9
AEsir777 <lkb2438834817@gmail.com>
2022-11-12 15:24:13 -0500

    add UI for activity logs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// test fiels
+const ALList = [
+  {
+    activity: "Do the cleaning",
+    description: "Clean the living room and bed room",
+    is_compeleted: true,
+    due_time: '2022 October 11 19:00',
+    priority: 'list-group-item-light',
+  },
+  {
+    activity: "Study CS 1023",
+    description: "Prepare for the mid term",
+    is_compeleted: false,
+    due_time: '2022 October 23 12:00',
+    priority: 'list-group-item-danger',
+  },
+  {
+    activity: "Study MATH 4233",
+    description: "Finish first chapter and do the after chapter problems",
+    is_compeleted: false,
+    due_time: '2022 October 24 14:00',
+    priority: 'list-group-item-warning',
+  },
+  {
+    activity: "Finish ARTS 342 Assignment 4",
+    description: "",
+    is_compeleted: false,
+    due_time: '2022 October 29 19:00',
+    priority: 'list-group-item-danger',
+  },
+];
+
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logs: ALList,
+    };
+  }
+
+  renderLogs(is_compeleted) {
+    const completed = this.state.logs.filter((log) =>
+      log.is_compeleted === is_compeleted,
+    );
+
+    return completed.map((log) => {
+      return (
+        <li
+          class={`list-group-item d-flex justify-content-between align-item-start list-group-item-action ${log.priority}`}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <input
+            class="form-check-input me-1"
+            type="checkbox"
+            value=""
+            defaultChecked={is_compeleted}
+          ></input>
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">{log.activity}</div>
+          </div>
+        </li>
+      );
+    });
+  }
 
-export default App;
- */
-
-class ListLog extends React.Component {
   render() {
-    
+    return (
+      <body>
+        <div class="container">
+          <h2 class="my-4"> Activity Logs </h2>
+          <div class="row">
+            <div class="col-md-7 col-sm-10 mx-auto p-0">
+              <ul class="list-group">{this.renderLogs(false)}</ul>
+            </div>
+            <div class="col-md-4 col-sm-10 mx-auto p-0">
+              <h3>completed </h3>
+              <ul class="list-group">{this.renderLogs(true)}</ul>
+            </div>
+          </div>
+        </div>
+      </body>
+    );
   }
 }
+
+export default List;
